775c499e2f179d33a5e71eae607cc3b7c4831dac
adekoerniawan <ade.kurniawan44@gmail.com>
2021-09-27 10:35:59 +0700

    'rvisi1'

---
+++ practical2.py
@@ -0,0 +1,13 @@
+data = {1,2,3, 4}
+
+
+data.add(7)
+print(data)
+number_set = {2, 4,  5, 5}
+data = data & number_set
+print(data)
+number_set.add('9')
+
+list_number = [1, 2, 3, 3, 2, 1]
+print(set(list_number))
+
